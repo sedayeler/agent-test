ab5e43f2f962fcf6e4cb5ed8e5d93666a0da4c56
Seda Yeler (Dogus Teknoloji) <Seda.Yeler@d-teknoloji.com.tr>
2025-10-20 15:55:42 +0300

    Add project files.

---
+++ AgentTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using AgentTest;
+
+Calculator calc = new Calculator();
+
+Console.WriteLine("2 + 3 = " + calc.Add(2, 3));
+Console.WriteLine("5 - 3 = " + calc.Subtract(5, 3));
+Console.WriteLine("6 * 2 = " + calc.Multiply(6, 2));
+
+try
+{
+    Console.WriteLine("10 / 2 = " + calc.Divide(10, 2));
+}
+catch (DivideByZeroException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
